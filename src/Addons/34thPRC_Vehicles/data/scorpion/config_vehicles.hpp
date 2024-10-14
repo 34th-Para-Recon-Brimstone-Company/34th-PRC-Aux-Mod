2144910dffa0565320ec30fda1f552689f25982f
Lupus590 <lupussolitarius590@gmail.com>
2024-10-14 23:21:53 +0100

    Move to woodland

--- src/Addons/34thPRC_Vehicles/data/scorpion/config_vehicles.hpp
+++ src/Addons/34thPRC_Vehicles/data/scorpion/config_vehicles.hpp
@@ -9,13 +9,13 @@ class 34thPRC_Vehicles_Scorpion : DMNS_M808B_F
 
 	hiddenSelectionsTextures[]=
 	{
-		"34thPRC_Vehicles\data\scorpion\M808B_Hull_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Turret_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Track_Housing_Front_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Track_Housing_Rear_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Commander_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Details_EXT_co.paa",
-		"34thPRC_Vehicles\data\scorpion\M808B_Details_EXT_2_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Hull_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Turret_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Track_Housing_Front_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Track_Housing_Rear_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Commander_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Details_EXT_co.paa",
+		"34thPRC_Vehicles\data\scorpion\woodland\M808B_Details_EXT_2_co.paa",
 		"DMNS\DMNS_Armour\M808B_Scorpion\data\M808B_Track_Int_co.paa"
 	};
 	textureList[] = {};
